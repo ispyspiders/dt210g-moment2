0bf66bfbf281df59641eaced5ed6c6e1e6f97409
Kajsa Classon <ispyspiders@gmail.com>
2025-02-05 11:39:59 +0100

    lagt till type Status i interface för todo

--- src/interfaces/TodoInterface.ts
+++ src/interfaces/TodoInterface.ts
@@ -1,6 +1,10 @@
 // Interface för Todo
+export type Status  = 'ej påbörjad' | 'pågående' | 'avklarad'
+
+
 export interface Todo {
+  id?: number,
   title: string,
   description?: string | null,
-  status: 'ej påbörjad' | 'pågående' | 'avklarad'
+  status: Status
 }
